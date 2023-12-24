02fdc75230e313cb296b025938a7df5bf6cec2d9
bruno8-alt <bini9399@gmail.com>
2023-12-24 10:21:42 +0300

    Final Changes

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -4,20 +4,47 @@ function getComputerChoice(){
  result = result.toLowerCase();
  return result;
 }
-
 function playRound(playerSelection, computerSelection){
     if(playerSelection === computerSelection){
         return `It is a Draw!`;
     }
-    else if(playerSelection === "rock" && computerSelection === "scissors"){
+    else if(
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock")
+        ) {
         return  `YOU WIN! ${playerSelection} beats ${computerSelection}.`;       
     }
-    else{
+    else if((playerSelection === "rock" && computerSelection === "paper") ||
+            (playerSelection === "scissors" && computerSelection === "rock") ||
+            (playerSelection === "paper" && computerSelection === "scissors")
+        ) {
         return `YOU LOSE! ${computerSelection} beats ${playerSelection}.`;    
-    }    
-}
-const playerSelection = "rock";
+    }}
+function game(){
+let playerScore = 0;
+let computerScore = 0;
+    for(i = 0; i < 5; i++)
+     {
+     let roundWinner = playRound(playerSelection, getComputerChoice());
+     console.log(roundWinner);
+     if(roundWinner.includes("WIN")){
+        playerScore++;
+       } 
+      else if(roundWinner.includes("LOSE")){
+        computerScore++; 
+    }}
+    if(playerScore > computerScore){
+        return `PLAYER WON!  Score: ${playerScore}-${computerScore}.`;
+     }
+    else if(playerScore < computerScore){
+        return `COMPUTER WON! Score: ${computerScore}-${playerScore}.`; 
+     }
+    else{
+        return `Draw!!! Score : ${playerScore}-${computerScore}.`;
+     }}
+let playerSelection = prompt("Enter either Rock, Paper or Scissors to play a random of 5 matches against the Computer!");
+playerSelection = playerSelection.toLowerCase();
 const computerSelection = getComputerChoice();
-console.log("Player choice =" + " " + playerSelection);
-console.log("Computer choice =" + " " + computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+console.log(game());
+
