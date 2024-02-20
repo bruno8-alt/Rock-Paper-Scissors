685feee452d86a71d9c01a758bc0efc684ea9d4d
bruno8-alt <bini9399@gmail.com>
2024-02-20 18:33:35 +0300

    Final Js

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,54 +1,77 @@
 function getComputerChoice(){
- let Choices = ["Rock","Paper", "Scissors"]; 
- let result = Choices[Math.floor(Math.random()* Choices.length)];
- result = result.toLowerCase();
- return result;
+    let choices = ["Rock", "Paper", "Scissors"]
+    let result = choices[Math.floor(Math.random() * choices.length)];
+    result = result.toLowerCase();
+    return result;  
+    }
+let playerSelection;
+let computerSelection;
+count = 0;
+playerScore = 0;
+computerScore = 0;
+const section = document.querySelector("section");
+const computerChoice = document.createElement("p");
+const para = document.createElement("p");
+const paraWinner = document.createElement("p");
+
+function btnClicked(value){
+count++;
+playerSelection = value;
+computerSelection = getComputerChoice();
+let display = console.log(count, playerSelection, computerSelection);
+let roundWinner = playRound(playerSelection, computerSelection);
+console.log(roundWinner);
+computerChoice.textContent = ( count + "." + "ComputerSelection = " + computerSelection);
+para.textContent = (roundWinner);
+section.appendChild(computerChoice);
+section.appendChild(para);
+if(roundWinner.includes("WIN")){
+    playerScore++;
 }
+else if(roundWinner.includes("LOSE")){
+    computerScore++;
+}
+if(count===5){
+    displayScore(playerScore, computerScore);
+    count=0;
+    playerScore=0;
+    computerScore=0;
+}
+}
+
 function playRound(playerSelection, computerSelection){
-    if(playerSelection === computerSelection){
-        return `It is a Draw!`;
+    if (playerSelection === computerSelection){
+        return `It is a Draw`;
     }
-    else if(
+    else if (
+        (playerSelection === "rock" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "rock") 
+        ){
+         return `You LOSE. ${computerSelection} beats ${playerSelection}`;
+    }
+    else if (
         (playerSelection === "rock" && computerSelection === "scissors") ||
-        (playerSelection === "scissors" && computerSelection === "paper") ||
-        (playerSelection === "paper" && computerSelection === "rock")
-        ) {
-        return  `YOU WIN! ${playerSelection} beats ${computerSelection}.`;       
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper") 
+        ){
+       return `You WIN. ${playerSelection} beats ${computerSelection}`;
     }
-    else if((playerSelection === "rock" && computerSelection === "paper") ||
-            (playerSelection === "scissors" && computerSelection === "rock") ||
-            (playerSelection === "paper" && computerSelection === "scissors")
-        ) {
-        return `YOU LOSE! ${computerSelection} beats ${playerSelection}.`;    
+    else{}
     }
-    else{} 
-    }
-let playerScore = Number();
-let computerScore = Number();
-
-function game(playerSelection, computerSelection){
-     let roundWinner = playRound(playerSelection, computerSelection);
-     console.log(roundWinner);
-     if(roundWinner.includes("WIN")){
-        playerScore++;
-       } 
-      else if(roundWinner.includes("LOSE")){
-        computerScore++; 
-    }}
-for(i = 0; i < 5; i++)
-     {
-        playerSelection = prompt("Enter either Rock, Paper or Scissors to play a random of 5 matches against the Computer!");
-        playerSelection = playerSelection.toLowerCase();
-        computerSelection = getComputerChoice();
-        console.log(game(playerSelection, computerSelection));      
-     }
-if(playerScore > computerScore){
-        console.log(`PLAYER WON!  Score: ${playerScore}-${computerScore}.`);
-     }
-else if(playerScore < computerScore){
-        console.log(`COMPUTER WON! Score: ${computerScore}-${playerScore}.`);
-     }
-else{
-        console.log(`Draw!!! Score : ${playerScore}-${computerScore}.`);
-     }
-
+    
+    function displayScore(playerScore, computerScore){
+        if(playerScore > computerScore){
+            console.log(`Player Won. ${playerScore} - ${computerScore}`);
+            paraWinner.textContent = `Player Won. ${playerScore} - ${computerScore}`
+        }
+        else if(playerScore < computerScore){
+            console.log(`Computer Won. ${computerScore} - ${playerScore}`);
+            paraWinner.textContent = `Computer Won. ${computerScore} - ${playerScore}`;
+        }
+        else{
+            console.log(`DRAW!`);
+            paraWinner.textContent = `DRAW! ${computerScore} - ${playerScore}`;
+        }
+    }  
+    section.appendChild(paraWinner);
