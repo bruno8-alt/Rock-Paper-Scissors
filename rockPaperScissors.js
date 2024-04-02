7288304174e6f95aa7123d38570a5f8e2431aeaa
bruno8-alt <bini9399@gmail.com>
2024-04-02 19:17:04 +0300

    UI changes

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -6,72 +6,85 @@ function getComputerChoice(){
     }
 let playerSelection;
 let computerSelection;
-count = 0;
-playerScore = 0;
-computerScore = 0;
 const section = document.querySelector("section");
 const computerChoice = document.createElement("p");
-const para = document.createElement("p");
+const paraRound = document.createElement("p");
 const paraWinner = document.createElement("p");
-
-function btnClicked(value){
-count++;
-playerSelection = value;
-computerSelection = getComputerChoice();
-let display = console.log(count, playerSelection, computerSelection);
-let roundWinner = playRound(playerSelection, computerSelection);
-console.log(roundWinner);
-computerChoice.textContent = ( count + "." + "ComputerSelection = " + computerSelection);
-para.textContent = (roundWinner);
-section.appendChild(computerChoice);
-section.appendChild(para);
-if(roundWinner.includes("WIN")){
-    playerScore++;
-}
-else if(roundWinner.includes("LOSE")){
-    computerScore++;
-}
-if(count===5){
-    displayScore(playerScore, computerScore);
-    count=0;
-    playerScore=0;
-    computerScore=0;
-}
-}
-
+count = 0;
+let playerScore = 0;
+let computerScore = 0;
+const buttons = document.querySelectorAll(".buttons");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if(button.value === "rock"){
+       playerSelection = "rock";
+    }
+    else if(button.value === "paper"){
+       playerSelection = "paper";
+    }
+    else if(button.value === "scissors"){
+       playerSelection = "scissors";
+    }
+    else{}
+    computerSelection = getComputerChoice();
+    let roundWinner = playRound(playerSelection, computerSelection);
+    if(roundWinner.includes("WIN")){
+        count++;
+        playerScore++;
+    }
+    else if(roundWinner.includes("LOSE")){
+        count++;
+        computerScore++;
+    }
+    else{count++;}
+    computerChoice.textContent = (count + ". ComputerSelection = " + computerSelection);
+    section.appendChild(computerChoice);
+    paraRound.textContent = (roundWinner);
+    section.appendChild(paraRound);
+    if(count===5){
+        displayScore(playerScore, computerScore);
+        count = 0;
+        playerScore=0;
+        computerScore=0;
+    }
+    else{paraWinner.remove();}
+  });
+});
 function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
-        return `It is a Draw`;
+        return `Round Winner: It is a Draw`;
     }
     else if (
         (playerSelection === "rock" && computerSelection === "paper") ||
         (playerSelection === "paper" && computerSelection === "scissors") ||
         (playerSelection === "scissors" && computerSelection === "rock") 
         ){
-         return `You LOSE. ${computerSelection} beats ${playerSelection}`;
+         return `Round Winner: You LOSE. ${computerSelection} beats ${playerSelection}`;
     }
     else if (
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper") 
         ){
-       return `You WIN. ${playerSelection} beats ${computerSelection}`;
+       return `Round Winner: You WIN. ${playerSelection} beats ${computerSelection}`;
     }
     else{}
     }
-    
     function displayScore(playerScore, computerScore){
         if(playerScore > computerScore){
-            console.log(`Player Won. ${playerScore} - ${computerScore}`);
-            paraWinner.textContent = `Player Won. ${playerScore} - ${computerScore}`
+            paraWinner.textContent = `RESULT = !!!YOU WON!!!. ${playerScore} - ${computerScore}`
+            paraWinner.style.backgroundColor = "Green";
+            section.appendChild(paraWinner);
         }
         else if(playerScore < computerScore){
-            console.log(`Computer Won. ${computerScore} - ${playerScore}`);
-            paraWinner.textContent = `Computer Won. ${computerScore} - ${playerScore}`;
+            paraWinner.textContent = `RESULT = You LOST!, Computer Won. ${computerScore} - ${playerScore}`;
+            paraWinner.style.backgroundColor = "Red";
+            section.appendChild(paraWinner);
         }
         else{
-            console.log(`DRAW!`);
-            paraWinner.textContent = `DRAW! ${computerScore} - ${playerScore}`;
-        }
-    }  
-    section.appendChild(paraWinner);
+            paraWinner.textContent = `RESULT = DRAW! ${computerScore} - ${playerScore}`;
+            paraWinner.style.backgroundColor = "Yellow";
+            section.appendChild(paraWinner);
+        }}
+    
+    
